2a59edab2befab01f2d34c19a5a955658f76fd97
githubawaq <97857484+perezbujeiro1980@users.noreply.github.com>
2023-03-20 12:07:49 -0300

    Agregar archivos de proyecto.

---
+++ ApiRestPractica/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiRestPractica.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ApiRestPractica/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiRestPractica.WebApiApplication" Language="C#" %>
